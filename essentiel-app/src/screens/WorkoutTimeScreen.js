212f381c9381c4ebea7ea9f9d7b8cce20fe550a7
Lemonochrome <80280962+Lemonochrme@users.noreply.github.com>
2024-01-23 16:31:18 +0000

    Save workout data to AsyncStorage

--- essentiel-app/src/screens/WorkoutTimeScreen.js
+++ essentiel-app/src/screens/WorkoutTimeScreen.js
@@ -1,29 +1,49 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Button, TextInput } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const WorkoutTimeScreen = ({ navigation, route }) => {
   const [workoutTime, setWorkoutTime] = useState('');
 
-  const handleSaveWorkoutTime = () => {
+  const handleSaveWorkoutTime = async () => {
     const durationMinutes = parseInt(workoutTime);
     if (isNaN(durationMinutes) || durationMinutes <= 0) {
       alert('Enter a valid workout duration.');
       return;
     }
-
-    // JSON object built from the user's input
-    const workoutData = {
-      type: route.params.workoutType, // Retrieve the workout type from the previous screen's parameter
-      intensity: route.params.workoutIntensity, // Retrieve the workout intensity from the previous screen's parameter
+  
+    const newWorkoutData = {
+      type: route.params.workoutType,
+      intensity: route.params.workoutIntensity,
       duration_minutes: durationMinutes,
-      date: new Date().toISOString(), // Today's date in YYYY-MM-DD format
+      date: new Date().toISOString(),
     };
-
-    console.log(workoutData);
-
+  
+    try {
+      // Retrieve existing workouts
+      const existingWorkouts = await AsyncStorage.getItem('workouts');
+      let workouts = existingWorkouts ? JSON.parse(existingWorkouts) : [];
+  
+      // Add the new workout to the array
+      workouts.push(newWorkoutData);
+  
+      // Save the updated array back to AsyncStorage
+      await AsyncStorage.setItem('workouts', JSON.stringify(workouts));
+  
+      // DEBUG: Print the updated workouts data structure to the console
+      console.log('Updated Workouts Data Structure:', workouts);
+  
+      alert('Workout data saved successfully!');
+    } catch (e) {
+      console.error('Error saving workout data:', e);
+      alert('Failed to save the data to the storage');
+    }
+  
     navigation.navigate('Home', { screen: 'workout', params: { tabIndex: 0 } });
   };
+  
 
   return (
     <View style={styles.container}>
