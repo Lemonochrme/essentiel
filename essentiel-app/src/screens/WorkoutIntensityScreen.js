4d380a35ad63471a86fb65ab20a71f81b9b63db4
Lemonochrome <80280962+Lemonochrme@users.noreply.github.com>
2024-01-23 09:29:06 +0000

    Update WorkoutIntensity with same 3 cards as WorkoutType

--- essentiel-app/src/screens/WorkoutIntensityScreen.js
+++ essentiel-app/src/screens/WorkoutIntensityScreen.js
@@ -1,20 +1,70 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Pressable, FlatList } from 'react-native';
 import { Text, Button } from 'react-native-paper';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
-const WorkoutIntensityScreen = ({ navigation }) => {
- return (
+const WorkoutIntensityScreen = ({ navigation, route }) => {
+  const [selectedCardIndex, setSelectedCardIndex] = useState(null);
+
+  const intensities = ['Light', 'Moderate', 'Heavy'];
+
+  const handleCardPress = (index) => {
+    setSelectedCardIndex(index);
+  };
+
+  const renderItem = ({ item, index }) => (
+    <Pressable
+      style={({ pressed }) => [
+        styles.card,
+        {
+          borderColor: selectedCardIndex === index ? 'white' : 'grey',
+        },
+      ]}
+      onPress={() => handleCardPress(index)}
+    >
+      <Text style={styles.cardText}>{item}</Text>
+      {selectedCardIndex === index ? (
+        <FontAwesome5Icon
+          name="check-circle"
+          size={20}
+          color="white"
+          style={styles.checkIcon}
+        />
+      ) : (
+        <FontAwesome5Icon
+          name="check-circle"
+          size={20}
+          color="grey"
+          style={styles.checkIcon}
+        />
+      )}
+    </Pressable>
+  );
+
+  return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
-        <Text>Select Workout Type</Text>
+        <Text>Select Workout Intensity</Text>
+      </View>
+      <View style={styles.cardsContainer}>
+        <FlatList
+          data={intensities}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
       </View>
       <View style={styles.buttonContainer}>
         <Button
           mode="contained"
           onPress={() => {
-            // Handle workout type selection and navigate to the Intensity screen
-            navigation.navigate('WorkoutTime');
+            const selectedIntensity =
+              selectedCardIndex !== null ? intensities[selectedCardIndex] : null;
+            navigation.navigate('WorkoutTime', {
+              workoutType: route.params.workoutType,
+              workoutIntensity: selectedIntensity,
+            });
           }}
+          disabled={selectedCardIndex === null}
         >
           Next
         </Button>
@@ -24,20 +74,39 @@ const WorkoutIntensityScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'space-between',
-      backgroundColor: '#1C1B1F',
-    },
-    contentContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 16,
-    },
-    buttonContainer: {
-      padding: 16,
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#1C1B1F',
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  cardsContainer: {
+    flex: 2,
+    paddingHorizontal: 16,
+    justifyContent: 'center',
+  },
+  card: {
+    flexDirection: 'row',
+    borderWidth: 2,
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    alignItems: 'center',
+  },
+  cardText: {
+    flex: 1,
+  },
+  checkIcon: {
+    marginLeft: 10,
+  },
+  buttonContainer: {
+    padding: 16,
+  },
+});
 
 export default WorkoutIntensityScreen;
